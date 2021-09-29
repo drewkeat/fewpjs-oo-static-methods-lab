14331bf34fd378d10d9fc61d74b0812a3c67ab7b
Drew Keat <thedrewkeat@gmail.com>
2021-09-28 22:29:52 -0600

    commit tree 3d1af0cc3bfc9438f9e4c481d40d456db8dd6997

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return `${string.charAt(0).toUpperCase()}` + `${string.slice(1)}`;
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, "");
+  }
+
+  static titleize(string) {
+    const leaveAlone = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const words = string.split(" ");
+    const cleaned = words.map((word, index) => {
+      if (!leaveAlone.includes(word) || index == 0) {
+        return this.sanitize(this.capitalize(word));
+      } else {
+        return word;
+      }
+    }, Formatter);
+    return cleaned.join(" ");
+  }
 }
